eda64040c24c49d5f9a4948e19495d39a27a85c1
HendricksK <kurvinhendricks@gmail.com>
2023-06-08 23:39:42 +0200

    EOD time to sleep

--- app/database/mysql.go
+++ app/database/mysql.go
@@ -3,18 +3,29 @@ package database
 import (
 	"log"
 	"database/sql"
+	"time"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var DB *sql.DB
+var database *sql.DB
 
 func Create() *sql.DB {
-	DB, err := sql.Open("mysql", "dev-sacos:CorsairTX850M@/sacos_dev")
+	database, err := sql.Open("mysql", "sacos-dev:CorsairTX850M@tcp(192.168.0.134:3309)/sacos_dev")
+	// id:password@tcp(your-amazonaws-uri.com:3306)/dbname
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	database.SetConnMaxLifetime(time.Minute * 3)
+	database.SetMaxOpenConns(10)
+	database.SetMaxIdleConns(10)
+
 	if err != nil {
 		log.Println(err)
 		// need to throw an error here
 	}
-	return DB
+
+	return database
 }
 
 func Close(conn *sql.DB) {
