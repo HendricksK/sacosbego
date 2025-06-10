d3fa8802268c2c11f19832b0a321d01c2c3c81c1
HendricksK <kurvinhendricks@gmail.com>
2025-06-10 22:45:08 +0200

    trying to figure out composer watch for building on the fly in dev

--- app/main.go
+++ app/main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	log.Println("Never going to give you up")
+	// log.Println("Never going to let you down, never going to turn around and desert you....")
+	log.Println("Joke Peralta")
 	log.Println("https://www.youtube.com/watch?v=LOZuxwVk7TU")
 
 	routes.Routes()
